bd7edf6cb037f1135448a24ae13be20507ab5d2b
Yuvraj Singh <zyuvraj53@gmail.com>
2021-11-06 22:26:41 +0530

    .

---
+++ MagicSquare/magicSquare.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define ullint unsigned long long int
+
+void initializeArray(ullint* array[], int size){
+
+}
+
+int main() {
+  ullint input;
+  scanf("%ullint", input);
+
+  ullint arr1[3];  
+  ullint arr2[3];
+  ullint arr3[3];
+
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      for(int k = 0; k < 3; k++){
+
+      }
+    }
+  }
+
+}
---
+++ QuickInvSqrt/main.c
@@ -0,0 +1,5 @@
+float Q_rsqrt(float number) { 
+  long i;
+  float x2, y;
+  const float threehalfs = 1.5F;
+}
