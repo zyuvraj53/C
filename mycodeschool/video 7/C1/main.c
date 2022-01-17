b17e1edb2039e9d36c550f7f486717499839bdb3
Yuvraj Singh <zyuvraj53@gmail.com>
2022-01-18 00:16:25 +0530

    .

---
+++ mycodeschool/video 7/C1/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+
+int SumOfElements(int A[]){
+  int i, sum = 0;
+  for(i = 0; i < sizeof(A); i)
+}
+
+int main(){
+  int A[] = {1, 2, 3, 4, 5};
+  int total = SumOfElements(A);
+  int size = sizeof(A);
+} 
